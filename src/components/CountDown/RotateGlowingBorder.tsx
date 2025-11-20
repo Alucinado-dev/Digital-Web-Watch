d4428fbf145e81cb77960f79b5e1687d2c644fd9
Lucino Campos <lucinogabriel1510@gmail.com>
2025-11-20 15:22:25 -0300

    ✨ add -  new component: rotate glowing border

---
+++ src/components/CountDown/RotateGlowingBorder.tsx
@@ -0,0 +1,54 @@
+type rotateGlowingBorderProps = {
+  circleSize: number
+  children: React.ReactNode
+}
+
+const RotateGlowingBorder = ({ circleSize, children }: rotateGlowingBorderProps) => {
+  return (
+    <div
+      className='relative flex items-center justify-center rounded-full bg-(--countdown-bg) p-16'
+      style={{ width: `${circleSize}px`, height: `${circleSize}px` }}
+    >
+      <div
+        className='absolute -z-10 animate-rotate-and-glow rounded-full'
+        style={{
+          width: `${circleSize + 15}px`,
+          height: `${circleSize + 15}px`,
+          background:
+            'linear-gradient(45deg, var(--countdown-border-1), var(--countdown-border-2), var(--countdown-border-3), var(--countdown-border-4))',
+
+          animation: 'rotateBorder 5s linear infinite, glowBorder 5s linear infinite',
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default RotateGlowingBorder
+
+/* @keyframes glowBorder {
+  0% {
+    filter: blur(20px);
+  }
+  50% {
+    filter: blur(5px);
+  }
+  100% {
+    filter: blur(20px);
+  }
+}
+
+@keyframes rotateBorder {
+  0% {
+    transform: rotate(0deg);
+  }
+  50% {
+    transform: rotate(180deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+ */
